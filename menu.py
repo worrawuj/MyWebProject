c872e6289eca956c1d130c34bd2e935d429fd387
worrawuj <98439984+worrawuj@users.noreply.github.com>
2022-01-26 11:08:20 +0700

    update hello

--- menu.py
+++ menu.py
@@ -1,16 +1,4 @@
 print('manin menu')
-print('hello')
-
-git config --global user.email "ratthaslip.booster@gmail.com"
-git config --global user.email "iotinvationlab"
-
-echo "# test" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/iotinvationlab/test.git
-git push -u origin main
-
-
-
+print('hello 1')
+print('hello 2')
+print('hello 3')
